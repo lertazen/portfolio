d1245762ff39b59c1d077e9627fa0599068c9f00
lertazen <111610632+lertazen@users.noreply.github.com>
2023-11-11 00:07:37 -0500

    Added skill icons

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,6 +2,7 @@
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { ABOUT_BODY } from '../constants';
 import { useRef } from 'react';
+import Image from 'next/image';
 
 const About = () => {
   const ref = useRef(null);
@@ -22,9 +23,34 @@ const About = () => {
       id='about'
       className='about_bg min-h-screen flex flex-col justify-center px-2'
     >
-      <div className='container mx-auto px-2'>
-        <div className='text-2xl'>About</div>
-        <div className='text-base md:text-xl p-1 md:p-0'>{ABOUT_BODY.body}</div>
+      <div className='container mx-auto px-2 grid grid-cols-4 md:grid-cols-12 gap-5'>
+        <div className='col-span-4 md:col-span-6'>
+          <div className='text-2xl'>About</div>
+          <div className='text-base md:text-xl p-1 md:p-0'>
+            {ABOUT_BODY.body}
+          </div>
+        </div>
+        <div className='col-span-4 md:col-span-6'>
+          <h3 className='text-xl'>Skills:</h3>
+          <div className='flex gap-3'>
+            {ABOUT_BODY.skills.map((skill, index) => (
+              <div className='relative'>
+                <Image
+                  key={index}
+                  src={skill.src}
+                  width={45}
+                  height={45}
+                  alt={skill.alt}
+                  className='hover:scale-125 hover:show skill_icon'
+                />
+
+                <div key={index} className='tooltip'>
+                  {skill.name}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </motion.div>
   );
--- app/constants/index.js
+++ app/constants/index.js
@@ -7,4 +7,51 @@ export const NAV_LINKS = [
 
 export const ABOUT_BODY = {
   body: "Hi, I'm Nathan, a full-stack developer with a knack for creating intuitive and dynamic user experiences. Trained in Computer Programming at Seneca College, I bring a unique mix of engineering precision and coding finesse to the tech table. Proficient in React, Next.js, and a suite of modern web technologies, I'm all about crafting accessible, efficient websites that drive user engagement. Outside of code, I'm a board game enthusiast and a continuous learner. I'm ready to join a team where collaboration, innovation, and growth are part of the daily routine.",
+  skills: [
+    {
+      src: 'html-5.svg',
+      alt: 'html5 icon',
+      name: 'html5',
+    },
+    {
+      src: 'css.svg',
+      alt: 'css icon',
+      name: 'css',
+    },
+    {
+      src: 'javascript.svg',
+      alt: 'javascript icon',
+      name: 'Javascript',
+    },
+    {
+      src: 'nodejs.svg',
+      alt: 'nodejs icon',
+      name: 'node.js',
+    },
+    {
+      src: 'mongodb.svg',
+      alt: 'mongodb icon',
+      name: 'mongoDB',
+    },
+    {
+      src: 'expressjs.svg',
+      alt: 'expressjs icon',
+      name: 'express.js',
+    },
+    {
+      src: 'react.svg',
+      alt: 'react icon',
+      name: 'react.js',
+    },
+    {
+      src: 'nextjs.svg',
+      alt: 'nextjs icon',
+      name: 'next.js',
+    },
+    {
+      src: 'tailwindcss.svg',
+      alt: 'tailwindcss icon',
+      name: 'tailwind css',
+    },
+  ],
 };
